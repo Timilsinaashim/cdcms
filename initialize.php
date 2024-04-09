20d06b2369ff4af8b7ed006d77dac4098cabe175
Timilsinaashim <ashimtimilsina052@gmail.com>
2024-04-09 21:09:06 +0545

    base url added

--- initialize.php
+++ initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/cdcms/');
+if(!defined('base_url')) define('base_url','http://54.90.77.56/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 
@@ -14,3 +14,9 @@ if(!defined('DB_NAME')) define('DB_NAME',"cdcms");
 // if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"adminadmin");
 // if(!defined('DB_NAME')) define('DB_NAME',"cdms");
 ?>
+
+<script>
+// Printing base_url to the browser console
+console.log("connection part here");
+console.log("<?php echo defined('base_url') ? base_url : ''; ?>");
+</script>
